e3d5f5fbea25b0cab7a6adc929c90a1045ac24d7
Martin Svejda <svejdam13@gmail.com>
2020-10-18 22:50:47 +0100

    script.js

--- script.js
+++ script.js
@@ -8,10 +8,6 @@ const calculateWealthBtn = document.getElementById('calculate-wealth');
 let data = [];
 
 
-getRandomUser();
-getRandomUser();
-getRandomUser();
-
 // fetch random user and add money
 async function getRandomUser(){
   const res = await fetch('https://randomuser.me/api');
@@ -27,4 +23,24 @@ async function getRandomUser(){
 //Add new obj to data arr
 function addData(obj) {
   data.push(obj);
+
+  updateDOM();
 }
+// function DOM
+function updateDOM(providedData = data){
+  // Clear main div
+  main.innerHTML = '<h2><strong>Person</strong> Wealth</h2>';
+
+  providedData.forEach( item => {
+    const element = document.createElement('div');
+    element.classList.add('person');
+    element.innerHTML = `<strong>${item.name}</strong> ${formatMoney(item.money)}`;
+    main.appendChild(element);
+  });
+}
+// Format number as money
+function formatMoney(number){
+  return  '$ ' + number.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,'); // from Stack overflow : format numbers as currency
+}
+// Event Listeners
+addUserBtn.addEventListener('click', getRandomUser)
