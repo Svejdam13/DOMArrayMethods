ee0f76bd7adb90eccb852ca1c4b81abb624e56ee
Martin Svejda <svejdam13@gmail.com>
2020-10-23 20:24:29 +0100

    script.js

--- script.js
+++ script.js
@@ -56,9 +56,17 @@ function sortByRichest(){
 }
 // Filter only millionaires
 function showMillionaires() {
-  data = data.filter(user => user.money > 1000000)
+  data = data.filter(user => user.money > 1000000);
   updateDOM();
   }
+// Summary
+function calculateWealth() {
+  const wealth = data.reduce((acc,user) => (acc += user.money), 0);
+  const wealthEl = document.createElement('div');
+  wealthEl.innerHTML = `<h3>Total Wealth: <strong>${formatMoney(wealth)}</strong></h3>`;
+  main.appendChild(wealthEl);
+   
+}
 
 
 // Event Listeners
@@ -66,4 +74,5 @@ addUserBtn.addEventListener('click', getRandomUser);
 doubleBtn.addEventListener('click', doubleMoney);
 sortBtn.addEventListener('click', sortByRichest);
 showMillionairesBtn.addEventListener('click', showMillionaires);
+calculateWealthBtn.addEventListener('click', calculateWealth);
 
